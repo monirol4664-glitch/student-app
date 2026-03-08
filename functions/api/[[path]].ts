bfe04429ad3c148a9024c642b772425fe8364693
monirol4664-glitch <monirol4664@gmail.com>
2026-03-08 06:01:50 +0600

    Delete functions/api directory

--- functions/api/[[path]].ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/cloudflare-pages'
-
-const app = new Hono<{ Bindings: { DB: D1Database } }>().basePath('/api')
-
-// Test route to see if the app is alive
-app.get('/ping', (c) => c.json({ message: 'Student App Backend is Live!' }))
-
-export const onRequest = handle(app)
