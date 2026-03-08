6cd30d5b9fa1afcbfaea7dfc0853952faa8bf7c4
monirol4664-glitch <monirol4664@gmail.com>
2026-03-08 16:35:22 +0600

    update

--- functions/api/login.js
+++
@@ -1,7 +0,0 @@
-export async function onRequestPost({request}) {
-  // TEMP TEST - Ignore database
-  const headers = new Headers();
-  headers.append('Set-Cookie', `session=test123; HttpOnly; Secure; SameSite=Strict; Max-Age=86400`);
-  headers.append('Content-Type', 'application/json');
-  return new Response(JSON.stringify({success: true}), {status: 200, headers});
-}
