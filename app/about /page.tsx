1d49b17e769e468de26ffc238816ce0ae9ec1487
monirol4664-glitch <monirol4664@gmail.com>
2026-03-10 05:36:30 +0600

    update

--- app/about /page.tsx
+++
@@ -1,8 +0,0 @@
-export default function About() {
- return (
-  <div>
-   <h1>About Our University</h1>
-   <p>This university provides quality education.</p>
-  </div>
- )
-}
--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
- return (
-  <main>
-   <h1>University Portal</h1>
-   <p>Welcome to the university website.</p>
-  </main>
- )
-}
