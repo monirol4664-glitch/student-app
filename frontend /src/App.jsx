b0ee496c1f2a82edf3ec469ad69b4d9ee8ef8c73
monirol4664-glitch <monirol4664@gmail.com>
2026-03-10 01:19:25 +0600

    update

--- frontend /src/App.jsx
+++ frontend /src/App.jsx
@@ -3,12 +3,12 @@ import Register from "./pages/register.jsx"
 
 export default function App() {
   return (
-    <div>
+    <div className="container">
       <h1>University System</h1>
 
       <Login />
+      <hr />
       <Register />
-
     </div>
   )
 }
