9cf0d276c88fd0c32e9a86490138abaeaeac4226
monirol4664-glitch <monirol4664@gmail.com>
2026-03-10 14:15:16 +0600

    update worker.js

--- api/worker.js
+++ api/worker.js
@@ -4,24 +4,32 @@ export default {
     const url = new URL(request.url)
     const path = url.pathname.replace(/\/$/, "")
 
-    if (path === "/api/students") {
+    const headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type"
+    }
 
+    if (request.method === "OPTIONS") {
+      return new Response(null, { headers })
+    }
+
+    if (path === "/api/students") {
       const { results } = await env.DB.prepare(
         "SELECT * FROM students"
       ).all()
 
-      return Response.json(results)
+      return new Response(JSON.stringify(results), { headers })
     }
 
     if (path === "/api/courses") {
-
       const { results } = await env.DB.prepare(
         "SELECT * FROM courses"
       ).all()
 
-      return Response.json(results)
+      return new Response(JSON.stringify(results), { headers })
     }
 
-    return new Response("API running")
+    return new Response("API running", { headers })
   }
 }
