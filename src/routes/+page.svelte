f3d845db7eb75f4e2daa090a851a25dec642f6f7
monirol4664-glitch <monirol4664@gmail.com>
2026-03-10 06:39:34 +0600

    update

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  let name = "Cloudflare";
+</script>
+
+<main style="text-align:center;margin-top:100px">
+  <h1>Hello from SvelteKit 🚀</h1>
+  <p>Running on {name} Pages</p>
+
+  <button on:click={() => name = "SvelteKit"}>
+    Click Me
+  </button>
+</main>
