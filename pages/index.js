62395b346bd45a488cb2148d49b02f37a2859c0a
monirol4664-glitch <monirol4664@gmail.com>
2026-03-10 06:14:53 +0600

    update

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign:"center", marginTop:"100px", fontFamily:"Arial"}}>
+      <h1>Next.js deployed on Cloudflare Pages 🚀</h1>
+      <p>If you see this page, deployment works!</p>
+    </div>
+  )
+}
