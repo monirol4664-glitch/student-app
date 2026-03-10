d94dace8a1db4226e71f27ef206385fc7daf1f83
monirol4664-glitch <monirol4664@gmail.com>
2026-03-10 14:00:02 +0600

    update worker.js

--- api/worker.js
+++ api/worker.js
@@ -2,9 +2,9 @@ export default {
   async fetch(request, env) {
 
     const url = new URL(request.url)
+    const path = url.pathname.replace(/\/$/, "")
 
-    // GET STUDENTS
-    if (url.pathname === "/api/students" && request.method === "GET") {
+    if (path === "/api/students") {
 
       const { results } = await env.DB.prepare(
         "SELECT * FROM students"
@@ -13,22 +13,7 @@ export default {
       return Response.json(results)
     }
 
-    // ADD STUDENT
-    if (url.pathname === "/api/students" && request.method === "POST") {
-
-      const data = await request.json()
-
-      await env.DB.prepare(
-        "INSERT INTO students (name,email,department) VALUES (?,?,?)"
-      )
-      .bind(data.name, data.email, data.department)
-      .run()
-
-      return Response.json({ success: true })
-    }
-
-    // GET COURSES
-    if (url.pathname === "/api/courses" && request.method === "GET") {
+    if (path === "/api/courses") {
 
       const { results } = await env.DB.prepare(
         "SELECT * FROM courses"
