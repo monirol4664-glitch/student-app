b99e11321409cfbc0e1b7358ee3f82b5e29f5a86
monirol4664-glitch <monirol4664@gmail.com>
2026-03-10 07:08:05 +0600

    update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,32 +2,12 @@
   let count = 0;
 </script>
 
-<style>
-  main {
-    text-align: center;
-    margin-top: 100px;
-    font-family: Arial;
-  }
+<h1>SvelteKit + Cloudflare 🚀</h1>
 
-  button {
-    padding: 10px 20px;
-    border-radius: 6px;
-    border: none;
-    background: #2563eb;
-    color: white;
-    cursor: pointer;
-  }
+<button on:click={() => count++}>
+  Click {count}
+</button>
 
-  button:hover {
-    background: #1d4ed8;
-  }
-</style>
+<br><br>
 
-<main>
-  <h1>SvelteKit + Cloudflare 🚀</h1>
-  <p>Button clicked {count} times</p>
-
-  <button on:click={() => count++}>
-    Click Me
-  </button>
-</main>
+<a href="/about">Go to About Page</a>
