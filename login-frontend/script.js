3d7d4379890269bef344c56002a0bc644ad5cef7
monirol4664-glitch <monirol4664@gmail.com>
2026-03-11 06:20:57 +0600

    update

---
+++ login-frontend/script.js
@@ -0,0 +1,43 @@
+const API="https://YOUR_WORKER_URL"
+
+async function signup(){
+
+const username=document.getElementById("username").value
+const password=document.getElementById("password").value
+
+await fetch(API+"/signup",{
+method:"POST",
+headers:{"Content-Type":"application/json"},
+body:JSON.stringify({username,password})
+})
+
+alert("Signup success")
+location.href="login.html"
+
+}
+
+async function login(){
+
+const username=document.getElementById("username").value
+const password=document.getElementById("password").value
+
+const res = await fetch(API+"/login",{
+method:"POST",
+headers:{"Content-Type":"application/json"},
+body:JSON.stringify({username,password})
+})
+
+const data = await res.json()
+
+if(data.success){
+
+localStorage.setItem("token","logged")
+location.href="dashboard.html"
+
+}else{
+
+alert("Invalid login")
+
+}
+
+}
