47f16e429c7f5a78e844b8b0ac5c3ae56cc574e7
monirol4664-glitch <monirol4664@gmail.com>
2026-04-18 00:02:07 +0600

    Refactor index.js to define and render App component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return <h1>Hello from my custom app!</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
