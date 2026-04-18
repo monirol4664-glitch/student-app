147319a8a9936027a6b86e0963037398aa1adfaa
monirol4664-glitch <monirol4664@gmail.com>
2026-04-18 09:16:28 +0600

    Update Capacitor config with new app details

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,11 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.myapp.photogallery',
-  appName: 'Photo Gallery',
-  webDir: 'build',
-  android: {
-    allowMixedContent: true,
-    webContentsDebuggingEnabled: true
+  appId: 'com.yourcompany.yourapp',
+  appName: 'Your App Name',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
   }
 };
 
